7ae273bf27181c072c59f49e2ece8d7b9417b884
Noel Kujur <lovenoelkujur@gmail.com>
2024-06-13 21:55:26 +0530

    Login page created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import AddNewDoctor from "./components/AddNewDoctor";
+import AddNewAdmin from "./components/AddNewAdmin";
+import Messages from "./components/Messages";
+import Doctors from "./components/Doctors";
+import Sidebar from "./components/Sidebar";
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Context } from "./index";
+import axios from 'axios';
+import "./App.css";
 
-function App() {
+const App = () => {
+
+  const { isAuthenticated, setIsAuthenticated, setUser } = useContext(Context);
+
+  useEffect(() => {
+
+    const fetchUser = async() => {
+      try {
+        const response = await axios.get("http://localhost:9000/api/v1/user/admin/me", 
+        {withCredentials : true});
+        setIsAuthenticated(true);
+        setUser(response.data.user);
+      } 
+      catch (error) {
+        setIsAuthenticated(false);
+        setUser({});
+      }
+    };
+    fetchUser();
+
+  }, [isAuthenticated]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Router>
+        <Sidebar />
+        <Routes>
+          <Route path='/' element={<Dashboard />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/doctor/addnew' element={<AddNewDoctor />} />
+          <Route path='/admin/addnew' element={<AddNewAdmin />} />
+          <Route path='/messages' element={<Messages />} />
+          <Route path='/doctors' element={<Doctors />} />
+        </Routes>
+        <ToastContainer position='top-center' />
+      </Router>
+    </>
+  )
 }
 
 export default App;
