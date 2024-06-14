808c9a08640073cbc249ec3ba353fc2dbfdeefa6
Noel Kujur <lovenoelkujur@gmail.com>
2024-06-14 14:40:50 +0530

    new admin sign up page complete

--- src/components/AddNewAdmin.js
+++ src/components/AddNewAdmin.js
@@ -1,8 +1,132 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { Context } from "../index";
+import { Navigate, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const AddNewAdmin = () => {
+
+  const {isAuthenticated, setIsAuthenticated} = useContext(Context);
+
+  // State
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [uid, setUid] = useState("");
+  const [dob, setDob] = useState("");
+  const [gender, setGender] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigateTo = useNavigate();
+
+  // Handle Add New Admin 
+  const handleAddNewAdmin = async(e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://localhost:9000/api/v1/user/admin/addnew", 
+        { firstName, lastName, email, phone, uid, dob, gender, password }, 
+        {
+          withCredentials : true, 
+          headers : {"Content-Type" : "application/json"}
+        }
+      );
+      toast.success(response.data.message);
+      setIsAuthenticated(true);
+      navigateTo("/");
+    } 
+    catch (error) {
+      toast.error(error.response.data.message);
+    }
+
+  };
+
+  if(!isAuthenticated){
+    return <Navigate to={"/login"} />
+  }
+
   return (
-    <div>AddNewAdmin</div>
+    <>
+      <section className='page'>
+        <div className='container form-component add-admin-form'>
+          <img src='../logo.png' alt='logo' className='logo'/>
+          <h1 className='form-title'>ADD NEW ADMIN</h1>
+          <form onSubmit={handleAddNewAdmin}>
+
+            <div>
+              <input 
+                type='text' 
+                placeholder='First Name' 
+                value={firstName} 
+                onChange={(e) => setFirstName(e.target.value)} 
+              />
+              <input 
+                type='text' 
+                placeholder='Last Name' 
+                value={lastName} 
+                onChange={(e) => setLastName(e.target.value)} 
+              />
+            </div>
+            
+            <div>
+              <input 
+                type='text' 
+                placeholder='Email' 
+                value={email} 
+                onChange={(e) => setEmail(e.target.value)} 
+              />
+              <input 
+                type='number' 
+                placeholder='Phone Number' 
+                value={phone} 
+                onChange={(e) => setPhone(e.target.value)} 
+              />
+            </div>
+            
+            <div>
+              <input 
+                type='number' 
+                placeholder='UID' 
+                value={uid} 
+                onChange={(e) => setUid(e.target.value)} 
+              />
+              <input 
+                type='date' 
+                placeholder='Date of Birth' 
+                value={dob} 
+                onChange={(e) => setDob(e.target.value)} 
+              />
+            </div>
+            
+            <div>
+              <select value={gender} onChange={(e) => setGender(e.target.value)}>
+                <option value="">Select Gender</option>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+              </select>
+              <input 
+                type='password' 
+                placeholder='Password' 
+                value={password} 
+                onChange={(e) => setPassword(e.target.value)} 
+              />
+            </div>
+
+            <div
+              style={{
+                justifyContent : "center",
+                alignItems : "center"
+              }}
+            >
+              <button type='submit'>ADD NEW ADMIN</button>
+            </div>
+
+          </form>
+        </div>
+      </section>
+    </>
   )
 }
 
