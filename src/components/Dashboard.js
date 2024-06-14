10c834ddee3b442164dbdb88bcb2e2c8981d5e4c
Noel Kujur <lovenoelkujur@gmail.com>
2024-06-14 23:32:14 +0530

    updated

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -17,7 +17,7 @@ const Dashboard = () => {
   useEffect(() => {
     const fetchAppointment = async() => {
       try {
-        const { data } = await axios.get("http://localhost:9000/api/v1/appointment/getall",
+        const { data } = await axios.get("https://hospital-management-system-backend-edeu.onrender.com/api/v1/appointment/getall",
           {withCredentials : true}
         );
         setAppointments(data.appointment)
@@ -35,7 +35,7 @@ const Dashboard = () => {
   useEffect(() => {
     const fetchDoctor = async() => {
       try {
-        const { data } = await axios.get("http://localhost:9000/api/v1/user/doctors", 
+        const { data } = await axios.get("https://hospital-management-system-backend-edeu.onrender.com/api/v1/user/doctors", 
           {withCredentials : true}
         );
         setDoctors(data.doctor); 
